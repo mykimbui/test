1b3eab32f982491ce740a477ac86c96b724b757c
My Kim Bui <mykimbuibui@gmail.com>
2017-01-08 11:47:58 +0100

    routes changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,28 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def poster
+  end
+
+  def cambio
+  end
+
+  def IFSB
+  end
+
+  def zuru
+  end
+
+  def car
+  end
+
+  def adposter
+  end
+
+  def dad
+  end
+
+  def dhl
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
     get 'about', to: 'pages#about'
-  resources :projects, only: [:show]
+    get 'poster', to: 'pages#poster'
+    get 'cambio', to: 'pages#cambio'
+    get 'IFSB', to: 'pages#IFSB'
+    get 'zuru', to: 'pages#zuru'
+    get 'car', to: 'pages#car'
+    get 'adposter', to: 'pages#adposter'
+    get 'dad', to: 'pages#dad'
+    get 'dhl', to: 'pages#dhl'
+  # resources :projects, only: [:show]
 end
